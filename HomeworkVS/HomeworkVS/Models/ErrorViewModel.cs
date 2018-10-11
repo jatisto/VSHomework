b9a5aac9f9594f829b8f688f4f8ac6d0d9631d72
Joni <j.atisto@gmail.com>
2018-10-11 12:59:01 +0600

    VsProjectHomeWork

---
+++ HomeworkVS/HomeworkVS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HomeworkVS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
