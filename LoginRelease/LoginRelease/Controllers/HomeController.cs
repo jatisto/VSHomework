9562f88492693485f10b253e4d5eb3923fb4fc19
Joni <j.atisto@gmail.com>
2018-10-11 16:15:02 +0600

    it Work

---
+++ LoginRelease/LoginRelease/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using LoginRelease.Data;
+using Microsoft.AspNetCore.Mvc;
+using LoginRelease.Models;
+
+namespace LoginRelease.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Details()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View(_context.Users.ToList());
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LoginRelease/LoginRelease/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LoginRelease.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
